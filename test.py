0d58e4969a713d3bc510975aa8dde189040139df
nikhil <tve22me096@cet.ac.in>
2025-09-27 01:55:38 +0530

    polynomial project

--- test.py
+++ test.py
@@ -1,7 +1,22 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Sep 26 22:28:14 2025
+ # SImulation of fluid flow
 
-@author: Lenovo
-"""
-print("nikhil")
+import numpy as np
+
+#Polynomial definition
+poly_p = np.array([-4, 7, -3, 9])
+
+#polynomial derivative
+p_der = np.polyder(poly_p)
+
+#Confiramtion of correct polynomial derivative
+print('Derivative of polynomial =', p_der )
+
+#Analytical result at x = 0
+p_der_eval = np.polyval(p_der, 0.0)
+print('Theorotical Deriative =', p_der_eval)
+
+#Numerical calculations using FDM (forward)
+x_0 = 0.0
+h = np.float64(0.25)
+
+
